e35ecf8c5c985263dd024ce7e73e7a5c8ad5c801
Arunjha8 <arunojha876@gmail.com>
2024-05-29 01:17:35 +0530

    ok

---
+++ Basic/Bubble.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class Bubble {
+    public static void main(String[] args) {
+        int[] t = {10, 20, 90, 50, 80};
+        int temp = 0;
+        for (int i = 0; i < t.length; i++) {
+            for (int j = i + 1; j < t.length; j++) {
+                if (t[i] > t[j]) {
+                    temp = t[j];
+                    t[j] = t[i];
+                    t[i] = temp;
+                }
+            }
+            System.out.println(t[i] + " ");
+        }
+    }
+}
