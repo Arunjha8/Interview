908bd37d0ea11047d58209cd881f4033bddb6b9a
Arunjha8 <arunojha876@gmail.com>
2024-07-07 10:57:39 +0530

    ok

---
+++ Collection/Comparater/Employee.java
@@ -0,0 +1,33 @@
+package Collection.Comparater;
+
+public class Employee {
+    private String name;
+    private int id;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + salary;
+    }
+}
---
+++ Collection/List/TestArrayList.java
@@ -0,0 +1,12 @@
+package Collection.List;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+    public static void main(String[] args) {
+        ArrayList l=new ArrayList();
+        l.add(10);
+        l.add(20);
+        System.out.println(l);
+    }
+}
