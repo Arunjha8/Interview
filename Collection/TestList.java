5b4b698f52d5ca4bea02dee2b155c9d925224434
Arunjha8 <arunojha876@gmail.com>
2024-07-07 19:27:49 +0530

    ok

--- Collection/TestList.java
+++ Collection/TestList.java
@@ -1,6 +1,8 @@
 package Collection;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.List;
 
 public class TestList {
@@ -16,12 +18,12 @@ public class TestList {
         System.out.println("List = " + l.get(2));
 
         l.remove(2);
+        System.out.println("List Remove = " + l);
+
+        l.set(2, "one");
         System.out.println("List = " + l);
 
-        l.set(2,"one");
-        System.out.println("List = " + l);
-
-        System.out.println("List indexOf = "+l.indexOf('b'));
-       // System.out.println(l.subList(0,3));
+        System.out.println("List indexOf = " + l.indexOf('b'));
+        System.out.println(l.subList(0, 4));
     }
 }
