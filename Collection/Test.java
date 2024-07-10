5620a44996212fd513fc877d5bbb3b94cc40c4f9
Arunjha8 <arunojha876@gmail.com>
2024-07-10 22:30:54 +0530

    ok

--- Collection/Test.java
+++ Collection/Test.java
@@ -7,21 +7,15 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class Test {
     public static void main(String[] args) {
-        Queue q = new LinkedList();
+        // Queue q = new LinkedList();
+        Queue q = new ArrayBlockingQueue(2);
 
         // Adding elements using add and offer
         q.add("Element 1");
         q.offer("Element 2");
+        // q.add("Element 3");           // add throws Exception
+        q.offer("Element 3");
 
-        System.out.println("LinkedList Queue: " + q);
-
-        Queue q1 = new ArrayBlockingQueue(2);
-
-        // Adding elements using add and offer
-        q1.add("Element 1");
-        q1.add("Element 2");
-        q1.offer("Element 3");   // add throws Exception
-
-        System.out.println(q1);
+        System.out.println(q);
     }
 }
