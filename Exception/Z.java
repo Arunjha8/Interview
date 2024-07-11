bfcb2f26ddb02dbc2caaf8486e97f1eaf6bb538b
Arunjha8 <arunojha876@gmail.com>
2024-07-11 10:48:59 +0530

    ok

--- Exception/Z.java
+++ Exception/Z.java
@@ -3,7 +3,7 @@ package Exception;
 public class Z {
     public static void main(String[] args) {
         try {
-            int c = 10 / 0;
+            int c = 8 / 0;
         } catch (Exception e) {
             System.out.println(e);
         }
