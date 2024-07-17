b59b6e06cad90aa86205584155f065e9bc60fbf6
Arunjha8 <arunojha876@gmail.com>
2024-07-17 22:05:27 +0530

    ok

--- StreamAPI/ArrayToStream.java
+++ StreamAPI/ArrayToStream.java
@@ -5,11 +5,12 @@ import java.util.stream.Stream;
 
 public class ArrayToStream {
     public static void main(String[] args) {
-        String[] s = {"abc", "def", "ghi", "jkl"};
+        String[] s = {"abc", "ghi", "jkl", "def"};
 
-        Stream stream = Arrays.stream(s);
+        //   Stream stream = Arrays.stream(s);
+        //   stream.sorted().forEach(System.out::println);
 
-        stream.sorted().forEach(System.out::println);
+        Arrays.stream(s).sorted().forEach(System.out::println);
 
         System.out.println("-------------------");
 
@@ -18,3 +19,4 @@ public class ArrayToStream {
         stream1.map(e -> e.toUpperCase()).forEach(e -> System.out.println(e));
     }
 }
+// System.out::println ka matlab hai "standard output stream ka println method call karo."
