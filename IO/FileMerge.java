c8bcba85a04bca0f189582d362d24c26a9134c1f
Arunjha8 <arunojha876@gmail.com>
2024-08-05 11:15:53 +0530

    ok

--- IO/FileMerge.java
+++ IO/FileMerge.java
@@ -4,23 +4,22 @@ import java.io.*;
 
 public class FileMerge {
     public static void main(String[] args) throws IOException {
-        PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\Arun\\OneDrive\\Desktop\\XYZ\\Murge.txt"));
+        PrintWriter out = new PrintWriter(new FileWriter("C:\\Users\\Arun\\OneDrive\\Desktop\\XYZ\\Murge.txt"));
         BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Arun\\OneDrive\\Desktop\\XYZ\\abc.txt"));
 
         String s = br.readLine();
         while (s != null) {
-            pw.write(s);
+            out.write(s);
             s = br.readLine();
         }
         br.close();
         BufferedReader br1 = new BufferedReader(new FileReader("C:\\Users\\Arun\\OneDrive\\Desktop\\XYZ\\abcd.txt"));
         String s1 = br1.readLine();
         while (s1 != null) {
-            pw.write(s1);
+            out.write(s1);
             s1 = br1.readLine();
         }
         br1.close();
-        pw.close();
-        //Arun
+        out.close();
     }
 }
